0cfa90102a0010e35fc128b0143b25dbab8d1dae
nattyemu <natnaelbayut@gmail.com>
2023-08-10 05:56:04 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the program that use puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
