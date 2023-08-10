c25f3a89b1522717663e0d0c7ce221e25b3477d6
nattyemu <natnaelbayut@gmail.com>
2023-08-10 06:29:57 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ #include <unistd.h>
+/**
+ * main -a program use fput function
+ *
+ * Return:1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (0);
+}
