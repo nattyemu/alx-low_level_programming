085a13811ba8e3ff62e9f749ba1e573e78a7b092
nattyemu <natnaelbayut@gmail.com>
2023-08-11 07:43:20 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - a function prints numbers with comma and space
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+	{
+	putchar(c + '0');
+
+	if (c == 9)
+	continue;
+
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
