48fb031544e9526d57273b279cba8f38c5576ef1
nattyemu <natnaelbayut@gmail.com>
2023-08-11 10:20:10 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,36 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - a function prints all possible combinations of two two-digit numbers
+ * main - a function prints hexadecimal
  *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	int tens1, ones1, tens2, ones2;
+	int p, q;
 
-	for (tens1 = 0; tens1 < 10; tens1++)
+	for (p = 0; p <= 98; p++)
 	{
-		for (ones1 = 0; ones1 < 10; ones1++)
-		{
-			for (tens2 = tens1; tens2 < 10; tens2++)
-			{
-				for (ones2 = ones1 + 1; ones2 < 10; ones2++)
-				{
-					putchar(tens1 + '0');
-					putchar(ones1 + '0');
-					putchar(' ');
-					putchar(tens2 + '0');
-					putchar(ones2 + '0');
-					if (tens1 == 9 && ones1 == 8 && tens2 == 9 && ones2 == 9)
-					continue;
+	for (q = p + 1; q <= 99; q++)
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(',');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 99)
+	continue;
 
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
