ac6b671eb1d2614b2b258b21d8ed2ac199ec7ec8
nattyemu <natnaelbayut@gmail.com>
2023-08-15 06:31:48 -0700

    7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+* print_last_digit - to check alphabet
+* @n: to be checked
+* Return: absolute value for number
+*/
+int print_last_digit(int n)
+{
+int last;
+
+last = n % 10;
+if (n < 0)
+{
+last = last * -1;
+}
+_putchar(last + '0');
+return (last);
+}
