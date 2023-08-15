cb322734169d7cf193a19d8987d804479d90cc72
nattyemu <natnaelbayut@gmail.com>
2023-08-15 06:22:08 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+* print_sign - to check alphabet
+* @n: to be checked
+* Return: 1 to positive or 0 to zero -1 to negative 
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}
