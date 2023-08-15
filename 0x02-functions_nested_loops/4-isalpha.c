f1849dc0930167e41241231ec5a15b1c0aedd79b
nattyemu <natnaelbayut@gmail.com>
2023-08-15 06:12:01 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /*
-* _islower - tocheck lowercase
-* _islower - to check lowercase alphabet
+* _isalpha - to check alphabet
 * @c: to be checked
 * Return: 1 lowercase or 0 everything else
 */
-int _islower(int c)
+int _isalpha(int c)
 {
-if (c >= 95 && c <= 122)
+if ((c >= 95 && c <= 122) || (c >= 65 && c <= 90))
 {
 return (1);
 }
