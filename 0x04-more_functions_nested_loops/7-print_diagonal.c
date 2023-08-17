dda5b9241318722a6228dacd2b956129b45bdde5
nattyemu <natnaelbayut@gmail.com>
2023-08-17 12:20:38 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print triangle
+ * @n: size of diagonal
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+int i, j;
+
+for (i = 0; i < n; i++)
+{
+for (j = 1; j <= i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
