3039a0a0e983df7140d4b09c17f4b968e20a3159
nattyemu <natnaelbayut@gmail.com>
2023-08-17 12:40:02 -0700

    8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: size of square
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+int i, j;
+
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+if (size <= 0)
+_putchar('\n');
+}
