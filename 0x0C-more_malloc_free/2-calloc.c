d6d1eacffc25ac8ad633e5f43e9e21a0d0d5b856
nattyemu <natnaelbayut@gmail.com>
2023-09-07 22:23:04 -0700

    1-string_nconcat

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _calloc - a function that allocates memory for an array
+ * @nmemb: number of members
+ * @size: size
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int i = 0, l = 0;
+char *p;
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+l = nmemb * size;
+p = malloc(l);
+if (p == NULL)
+return (NULL);
+
+while (i < l)
+{
+p[i] = 0;
+i++;
+}
+return (p);
+}
