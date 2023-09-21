67fdc94e5c7e11c0c29fdb1b81fbb9c728a00d8a
nattyemu <natnaelbayut@gmail.com>
2023-09-21 10:02:35 -0700

    all tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((contractor));
+
+/**
+ * first - prints a statement before main
+ * function is excited
+ */
+
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
